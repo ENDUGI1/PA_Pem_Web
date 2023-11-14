bd9e353b1d416d9c503a737022ed9ed8888819c7
Ahmad Nur Fauzan <113954428+ENDUGI1@users.noreply.github.com>
2023-11-14 09:44:25 +0800

    Add files via upload

---
+++ PA WEB/search.js
@@ -0,0 +1,18 @@
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+
+const sb = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("activate");
+  }
+});
+
