a8cce74d988a0e50f01451d3c7b7398fdc89af89
Abdullah Azam <abdullahazam640@gmail.com>
2023-11-16 12:57:51 +0800

    add: Hapus_pesan.php

---
+++ PA WEB/hapus_pesan.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if (!isset($_SESSION['login']) || ($_SESSION['login'] !== true) || (isset($_SESSION['username']) && $_SESSION['username'] !== 'admin')){
+    header('Location: index.php');
+    exit();
+}
+require "koneksi.php";
+$id = $_GET['id'];
+
+$result = mysqli_query($conn,"DELETE FROM data_kontak WHERE id = '$id'");
+
+if ($result) {
+    echo "
+    <script>
+        alert('Data berhasil Hapus!');
+        document.location.href = 'dashboard.php'
+    </script>";
+} else {
+    echo "
+    <script>
+        alert('Data Gagal Hapus!');
+        document.location.href = 'dashboard.php'
+    </script>";
+}
+
+?>
